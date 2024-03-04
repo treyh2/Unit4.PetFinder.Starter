b649a3630227a48df27b8e8131522e09130ff147
Your Name <your_email@example.com>
2024-03-04 17:50:53 -0600

    finished

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const app = express();
 
 const PORT = 8080;
 
+app.use(express.static('public'));
 // GET - / - returns homepage
 app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
     // serve up the public folder as static index.html file
-
 });
 
 // hello world route
@@ -21,32 +22,42 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.json(pets);
+    console.log('send pets')
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const owner = req.query.owner;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
-
+    if (pet) {
+      res.json(pet);
+    } else {
+      res.status(404).json({ error: "Pet not found"});
+    }
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
-    // get the name from the request
+  // get the name from the request
+  const name = req.params.name;
 
+  // find the pet in the pets array
+  const pet = pets.find(pet => pet.name === name);
 
-    // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === name);
-
-    // send the pet as a response
-
+  // send the pet as a response
+  if (pet) {
+      res.json(pet);
+  } else {
+      res.status(404).json({ error: 'Pet not found' });
+  }
 });
+
 
 app.listen(PORT, () => {
     console.log('Server is listening on port ' + PORT);
